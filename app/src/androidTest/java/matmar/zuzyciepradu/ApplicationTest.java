ab62c309f045ddac3425c0b72d8ab75bd4d54a0e
mateusz <mateuszbak3@gmail.com>
2016-04-21 17:23:44 +0200

    Initial commit, raw project

---
+++ app/src/androidTest/java/matmar/zuzyciepradu/ApplicationTest.java
@@ -0,0 +1,13 @@
+package matmar.zuzyciepradu;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
