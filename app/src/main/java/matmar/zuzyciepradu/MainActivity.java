bb3b866135c7ede2dd7588a096a75c2723b8de41
mateusz <mateuszbak3@gmail.com>
2016-04-25 15:39:34 +0200

    Adding date picker

--- app/src/main/java/matmar/zuzyciepradu/MainActivity.java
+++ app/src/main/java/matmar/zuzyciepradu/MainActivity.java
@@ -1,22 +1,56 @@
 package matmar.zuzyciepradu;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.appeaser.sublimepickerlibrary.SublimePicker;
+import com.appeaser.sublimepickerlibrary.datepicker.SelectedDate;
+import com.appeaser.sublimepickerlibrary.helpers.SublimeListenerAdapter;
+import com.appeaser.sublimepickerlibrary.helpers.SublimeOptions;
+import com.appeaser.sublimepickerlibrary.recurrencepicker.SublimeRecurrencePicker;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    SublimePicker sublimePicker;
+    SublimeListenerAdapter pickerListener = new SublimeListenerAdapter() {
+        @Override
+        public void onDateTimeRecurrenceSet(SublimePicker sublimeMaterialPicker, SelectedDate selectedDate, int hourOfDay, int minute, SublimeRecurrencePicker.RecurrenceOption recurrenceOption, String recurrenceRule) {
+
+        }
+
+        @Override
+        public void onCancelled() {
+        }
+    };
+    SublimeOptions sublimeOptions;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.action_date):
+                SublimePickerFragment pickerFrag = new SublimePickerFragment();
+                pickerFrag.setStyle(DialogFragment.STYLE_NO_TITLE,0);
+                pickerFrag.show(getSupportFragmentManager(), "SUBLIME_PICKER");
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
